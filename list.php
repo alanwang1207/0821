a1548d845ad6d6506acaf467b6dde5604dc59263
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-21 12:04:00 +0800

    關連式陣列轉換成物件

--- list.php
+++ list.php
@@ -1,10 +1,23 @@
+<!-- 使用關連式陣列轉換成物件練習 -->
 <?php
-    
+$scoreList =Array(100,50,90,95);
+$viewModal = [];
 
+foreach ($scoreList as $score )
+{
+    $student = (object)["score" => $score, "style" => "pass"];
+    if($score <60)
+    {
+        $student->style = "fail";
+    }
+    $viewModal[] = $student;
+}
 
-
+//$scoreList =[100 , 90];
+// $obj = (object)["firstName" => "Jermey", "lastName" => "Lin"];
+// echo $obj->lastName;
+// exit()
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,10 +25,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+     .fail
+       {
+         color:red;
+       }
+     .pass
+       {
+         color:black;
+       }
+    </style>
 </head>
 <body>
     <ul>
-        <li>100</li>
+        <?php foreach($viewModal as $student) {?>
+        <li class ="<?=$student->style?>"><?= $student->score?></li>
+        <?php } ?>
     </ul>
+    
 </body>
 </html>
